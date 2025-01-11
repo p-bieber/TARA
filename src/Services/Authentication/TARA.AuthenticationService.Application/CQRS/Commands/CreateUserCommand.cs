a256c47a4ebe37e845463ef6af1ec41ca3b8cb0c
p-bieber <103746884+p-bieber@users.noreply.github.com>
2025-01-11 17:11:25 +0100

    changed CreateUserCommand to record

--- src/Services/Authentication/TARA.AuthenticationService.Application/CQRS/Commands/CreateUserCommand.cs
+++ src/Services/Authentication/TARA.AuthenticationService.Application/CQRS/Commands/CreateUserCommand.cs
@@ -2,16 +2,4 @@
 
 namespace TARA.AuthenticationService.Application.CQRS.Commands;
 
-public class CreateUserCommand : IRequest<bool>
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Email { get; set; }
-
-    public CreateUserCommand(string username, string password, string email)
-    {
-        Username = username;
-        Password = password;
-        Email = email;
-    }
-}
+public record CreateUserCommand(string Username, string Password, string Email) : IRequest<bool>;
